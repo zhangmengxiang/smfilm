c7dbd8d797ae4efcde5d6eea6ed703ab2d79cb9e
ZhangMengXiang <zhangmengxiang000@163.com>
2021-02-20 10:24:26 +0800

    SSL证书处理

--- smfilm/src/main/java/com/shimeng/smfilm/controller/FilmSourceController.java
+++ smfilm/src/main/java/com/shimeng/smfilm/controller/FilmSourceController.java
@@ -4,10 +4,11 @@ import com.alibaba.fastjson.JSONObject;
 import com.shimeng.smfilm.common.BaseResp;
 import com.shimeng.smfilm.common.StatusMessage;
 import com.shimeng.smfilm.model.entity.FilmSource;
-import com.shimeng.smfilm.model.req.QueryFilmTypeReq;
 import com.shimeng.smfilm.model.req.QueryVideoUrlReq;
 import com.shimeng.smfilm.model.resp.MuObject;
 import com.shimeng.smfilm.service.FilmSourceService;
+import com.shimeng.smfilm.utils.SslUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
@@ -16,10 +17,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +26,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.io.IOException;
-import java.lang.reflect.Method;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -55,19 +54,21 @@ public class FilmSourceController {
             CloseableHttpResponse response = null;
 
             try {
+                String source = getRequest(url, 3000);
+
                 // 由客户端执行(发送)Get请求
-                response = httpClient.execute(httpGet);
+                //response = httpClient.execute(httpGet);
                 // 从响应模型中获取响应实体
-                HttpEntity responseEntity = response.getEntity();
-                System.out.println("响应状态为:" + response.getStatusLine());
-                if (responseEntity != null) {
-                    System.out.println("响应内容长度为:" + responseEntity.getContentLength());
-                    long contentLength = responseEntity.getContentLength();
-                    String source = EntityUtils.toString(responseEntity);
-                    System.out.println("响应内容为:" + source);
+                //HttpEntity responseEntity = response.getEntity();
+                //System.out.println("响应状态为:" + response.getStatusLine());
+                if (!"".equals(source)) {
+//                    System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+//                    long contentLength = responseEntity.getContentLength();
+//                    String source = EntityUtils.toString(responseEntity);
+//                    System.out.println("响应内容为:" + source);
 
                     // 为-1 则返回为html版
-                    if (contentLength == -1){
+                    if (source.contains("html")){
                         List<String> list = match(source, "iframe", "src");
                         String[] split = list.get(0).split("=");
                         muObject = new MuObject();
@@ -123,5 +124,49 @@ public class FilmSourceController {
             result.add(r);
         }
         return result;
+    }
+
+    /**
+     * get 请求封装
+     * @param url
+     * @param timeOut
+     * @return
+     * @throws Exception
+     */
+    public String getRequest(String url,int timeOut) throws Exception{
+        URL u = new URL(url);
+        if("https".equalsIgnoreCase(u.getProtocol())){
+            SslUtils.ignoreSsl();
+        }
+        URLConnection conn = u.openConnection();
+        conn.setConnectTimeout(timeOut);
+        conn.setReadTimeout(timeOut);
+        return IOUtils.toString(conn.getInputStream());
+    }
+
+    /**
+     * post 请求封装
+     * @param urlAddress
+     * @param args
+     * @param timeOut
+     * @return
+     * @throws Exception
+     */
+    public String postRequest(String urlAddress,String args,int timeOut) throws Exception{
+        URL url = new URL(urlAddress);
+        if("https".equalsIgnoreCase(url.getProtocol())){
+            SslUtils.ignoreSsl();
+        }
+        URLConnection u = url.openConnection();
+        u.setDoInput(true);
+        u.setDoOutput(true);
+        u.setConnectTimeout(timeOut);
+        u.setReadTimeout(timeOut);
+        OutputStreamWriter osw = new OutputStreamWriter(u.getOutputStream(), "UTF-8");
+        osw.write(args);
+        osw.flush();
+        osw.close();
+        u.getOutputStream();
+        return IOUtils.toString(u.getInputStream());
     }
 }
